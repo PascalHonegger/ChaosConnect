6a22085cb5eac021d5c3985ec4d4b825b60490a1
Pascal Honegger <pascal.honegger@bluewin.ch>
2021-03-06 21:00:28 +0100

    feat(frontend): add favicon based on tweemoji

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,12 +6,22 @@
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
+<footer>
+	<a href="https://github.com/twitter/twemoji">Twitter Emoji (Twemoji)</a> images licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0</a>
+</footer>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	footer {
+		text-align: center;
+		font-size: small;
+		padding: 1em;
 		margin: 0 auto;
 	}
 
