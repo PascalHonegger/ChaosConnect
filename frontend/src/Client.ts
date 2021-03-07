9f964709b9e419c0b2c513c79be53f73d99805f3
Pascal Honegger <pascal.honegger@bluewin.ch>
2021-03-07 01:31:01 +0100

    feat(frontend): sample gRPC client via echo.proto

---
+++ frontend/src/Client.ts
@@ -0,0 +1,18 @@
+import { EchoServiceClient } from "./gen/EchoServiceClientPb";
+import { EchoRequest, ServerStreamingEchoRequest } from "./gen/echo_pb";
+
+export default new EchoServiceClient("http://localhost:8080");
+
+export function newTodoRequest(message: string): EchoRequest {
+    const req = new EchoRequest();
+    req.setMessage(message);
+    return req;
+};
+
+export function newStreamingRequest(message: string, count: number = 10, interval: number = 500): ServerStreamingEchoRequest {
+    const req = new ServerStreamingEchoRequest();
+    req.setMessage(message);
+    req.setMessageCount(count);
+    req.setMessageInterval(interval);
+    return req;
+};
