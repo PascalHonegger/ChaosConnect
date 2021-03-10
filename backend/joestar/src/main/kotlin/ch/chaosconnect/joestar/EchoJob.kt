77c0aa2f8dfcc82c38eb22a3192fca8d0938b2d5
Pascal Honegger <pascal.honegger@bluewin.ch>
2021-03-10 23:27:30 +0100

    feat(backend): implement echo service, Joestar can talk to Rohan

---
+++ backend/joestar/src/main/kotlin/ch/chaosconnect/joestar/EchoJob.kt
@@ -0,0 +1,23 @@
+package ch.chaosconnect.joestar
+
+import ch.chaosconnect.rohan.EchoRequest
+import ch.chaosconnect.rohan.EchoServiceGrpcKt
+import io.micronaut.scheduling.annotation.Scheduled
+import kotlinx.coroutines.runBlocking
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import javax.inject.Singleton
+
+val logger: Logger = LoggerFactory.getLogger(EchoJob::class.java)
+
+@Singleton
+class EchoJob(private val client: EchoServiceGrpcKt.EchoServiceCoroutineStub) {
+    @Scheduled(fixedDelay = "10s")
+    fun sayHelloJob() {
+        runBlocking {
+            logger.info("Sending hello")
+            val response = client.echo(EchoRequest.newBuilder().setMessage("hello").build())
+            logger.info("Received response: ${response.message}")
+        }
+    }
+}
---
+++ backend/joestar/src/main/kotlin/ch/chaosconnect/joestar/RohanClients.kt
@@ -0,0 +1,20 @@
+package ch.chaosconnect.joestar
+
+import ch.chaosconnect.rohan.EchoServiceGrpcKt
+import io.grpc.ManagedChannel
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import javax.inject.Singleton
+
+
+@Factory
+internal class Clients {
+    @Singleton
+    fun reactiveStub(
+        @GrpcChannel("rohan") channel: ManagedChannel
+    ): EchoServiceGrpcKt.EchoServiceCoroutineStub {
+        return EchoServiceGrpcKt.EchoServiceCoroutineStub(
+            channel
+        )
+    }
+}
