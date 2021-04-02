7e00ee9e4b48c802b4510a08c1a06b633c7d0888
Pascal Honegger <pascal.honegger@bluewin.ch>
2021-04-02 20:13:13 +0200

    chore(backend): upgrade dependencies to latest

--- backend/buildSrc/src/main/kotlin/Dependencies.kt
+++ backend/buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,14 +1,14 @@
 object Versions {
-    const val kotlinVersion = "1.4.31"
+    const val kotlinVersion = "1.4.32"
     const val kotlinxVersion = "1.4.3"
     const val micronautVersion = "2.4.0"
     const val micronautGradlePluginVersion = "1.4.2"
     const val protobufGradlePluginVersion = "0.8.15"
     const val shadowVersion = "6.1.0"
     const val protocVersion = "3.15.5"
-    const val protocJavaVersion = "1.36.0"
+    const val protocJavaVersion = "1.36.1"
     const val protocKotlinVersion = "1.0.0"
-    const val mockkVersion = "1.10.6"
+    const val mockkVersion = "1.11.0"
     const val turbineVersion = "0.4.1"
     const val jvmTargetVersion = "15"
 }
