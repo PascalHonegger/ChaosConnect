a48eea7774c558e4bf3e1c934621a616d23a955d
Pascal Honegger <pascal.honegger@bluewin.ch>
2021-04-08 21:35:50 +0200

    feat(joestar): implement user api to Rohan

--- backend/joestar/src/main/kotlin/ch/chaosconnect/joestar/RohanClients.kt
+++ backend/joestar/src/main/kotlin/ch/chaosconnect/joestar/RohanClients.kt
@@ -1,6 +1,7 @@
 package ch.chaosconnect.joestar
 
 import ch.chaosconnect.api.rohan.GameServiceGrpcKt
+import ch.chaosconnect.api.rohan.UserServiceGrpcKt
 import io.grpc.ManagedChannel
 import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
@@ -13,6 +14,15 @@ internal class Clients {
         @GrpcChannel("rohan") channel: ManagedChannel
     ): GameServiceGrpcKt.GameServiceCoroutineStub {
         return GameServiceGrpcKt.GameServiceCoroutineStub(
+            channel
+        )
+    }
+
+    @Singleton
+    fun userService(
+        @GrpcChannel("rohan") channel: ManagedChannel
+    ): UserServiceGrpcKt.UserServiceCoroutineStub {
+        return UserServiceGrpcKt.UserServiceCoroutineStub(
             channel
         )
     }
