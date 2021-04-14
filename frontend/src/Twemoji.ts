5d21a629af6fb0a025c4b904a529d43ed62d2cc1
Pascal Honegger <pascal.honegger@bluewin.ch>
2021-04-14 19:11:17 +0200

    feat(doppio): implement login and register view

---
+++ frontend/src/Twemoji.ts
@@ -0,0 +1,23 @@
+import twitterEmoji from 'twemoji'
+import type { ParseObject } from 'twemoji';
+import { afterUpdate } from 'svelte'
+
+type TwemojiHow = Partial<ParseObject>;
+
+const defaults: TwemojiHow = {
+    folder: 'svg',
+    ext: '.svg'
+}
+
+export default function twemoji(node: HTMLElement, how: TwemojiHow = {}) {
+    how = {...defaults, ...how};
+    twitterEmoji.parse(node, how)
+    afterUpdate(() => {
+        twitterEmoji.parse(node, how)
+    })
+    return {
+        update() {
+            console.warn('Changing twemoji options after the action was mounted is not possible.')
+        }
+    }
+}
--- frontend/src/WebLoginClient.ts
+++ frontend/src/WebLoginClient.ts
@@ -1,11 +1,25 @@
 import {WebLoginServiceClient} from './gen/JoestarServiceClientPb';
-import {LoginRequest} from "./gen/authentication_pb";
+import {LoginRequest, PlayWithoutAccountRequest, RegisterRequest} from "./gen/authentication_pb";
 
 export default new WebLoginServiceClient("/api");
+
+export function newPlayRequest(displayName: string): PlayWithoutAccountRequest {
+    const loginRequest = new PlayWithoutAccountRequest();
+    loginRequest.setDisplayName(displayName);
+    return loginRequest;
+}
 
 export function newLoginRequest(username: string, password: string): LoginRequest {
     const loginRequest = new LoginRequest();
     loginRequest.setUsername(username);
     loginRequest.setPassword(password);
     return loginRequest;
+}
+
+export function newRegisterRequest(username: string, password: string, displayName: string): RegisterRequest {
+    const registerRequest = new RegisterRequest();
+    registerRequest.setUsername(username);
+    registerRequest.setPassword(password);
+    registerRequest.setDisplayName(displayName);
+    return registerRequest;
 }
