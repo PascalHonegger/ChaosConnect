536e06857586fd641ef68063ecc2db3807229e72
Pascal Honegger <pascal.honegger@bluewin.ch>
2021-04-21 10:51:40 +0200

    feat(doppio): try to renew token on start

---
+++ frontend/src/TokenRefresher.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import type {WebLoginServiceClient} from "./gen/JoestarServiceClientPb";
+    import type {Error} from "grpc-web";
+    import {StatusCode} from "grpc-web";
+    import {authMetadata, token} from "./Stores";
+    import {onMount} from "svelte";
+    import {newEmpty} from "./CommonClient";
+
+    export let client: WebLoginServiceClient;
+
+    const retryTimeout = 5000;
+
+    onMount(async () => {
+        await tryRenewToken();
+    });
+
+    async function tryRenewToken() {
+        try {
+            const tokenResponse = await client.renewToken(
+                newEmpty(),
+                $authMetadata
+            );
+            token.set(tokenResponse.getJwtToken());
+        } catch (error) {
+            if ((<Error>error).code === StatusCode.UNAUTHENTICATED) {
+                // Our current token is no longer valid => logout
+                token.unset();
+            } else {
+                console.warn(error);
+                setTimeout(async () => {
+                    await tryRenewToken();
+                }, retryTimeout);
+            }
+        }
+    }
+</script>
