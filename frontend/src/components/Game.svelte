6cb3d7e1e8f71bbd27f39dd056a531556d98eb2d
Marcel Joss <marcel.joss@ost.ch>
2021-05-05 10:31:55 +0200

    feat(frontend): implement faction select

--- frontend/src/components/Game.svelte
+++ frontend/src/components/Game.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
     import type { ClientReadableStream } from "grpc-web";
-    import Column from "./Grid.svelte";
     import { newEmpty } from "../lib/CommonClient";
-    import {
-        columns,
-        factions,
-        gameState
-    } from "../stores/GameState";
+    import { gameState, player } from "../stores/GameState";
     import type { GameUpdateEvent } from "../gen/game_pb";
     import type { ChaosConnectServiceClient } from "../gen/JoestarServiceClientPb";
     import { authMetadata } from "../stores/Auth";
     import { onMount } from "svelte";
-    import Piece from "./Piece.svelte";
-import Grid from "./Grid.svelte";
+    import Grid from "./Grid.svelte";
+    import PlayerList from "./PlayerList.svelte";
+    import FactionSelect from "./FactionSelect.svelte";
 
     export let client: ChaosConnectServiceClient;
 
@@ -25,46 +21,17 @@ import Grid from "./Grid.svelte";
     });
 </script>
 
-<div class="game">
-    <ul class="factions">
-        {#each [...$factions] as [faction, players]}
-            <li>
-                <Piece {faction} />
-                {#if players.length > 0}
-                    <ul class="players">
-                        {#each players as player}
-                            <li>
-                                {player.displayName}
-                                {#if player.disconnected}(Disconnected){/if}
-                            </li>
-                        {/each}
-                    </ul>
-                {:else}
-                    <div class="no-players-message">No Players yet</div>
-                {/if}
-            </li>
-        {/each}
-    </ul>
+{#if $player}
+    <div class="game">
+        <PlayerList />
 
-    <Grid />
-</div>
+        <Grid />
+    </div>
+{:else}
+    <FactionSelect />
+{/if}
 
 <style>
-    ul {
-        list-style: none;
-    }
-
-    .factions > li {
-        padding-top: 0.5rem;
-        border-bottom: 3px solid black;
-        padding-bottom: 0.5rem;
-    }
-
-    .players,
-    .no-players-message {
-        padding-left: var(--piece-size);
-    }
-
     .game {
         display: grid;
         grid-template-columns: 1fr 6fr;
