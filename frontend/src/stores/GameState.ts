d28f71069a5c934ecad02eaabf7de42eef64e1cf
Marcel Joss <marcel.joss@ost.ch>
2021-05-05 10:31:55 +0200

    feat(frontend): implement faction select

--- frontend/src/stores/GameState.ts
+++ frontend/src/stores/GameState.ts
@@ -1,6 +1,7 @@
-import { derived, Writable, writable } from "svelte/store";
-import { applyUpdate, initialGameState, Player } from "../lib/GameState";
-import { Faction, GameUpdateEvent } from "../gen/game_pb";
+import { derived, Readable, Writable, writable } from "svelte/store";
+import { applyUpdate, factions, initialGameState, Player } from "../lib/GameState";
+import type { Faction, GameUpdateEvent } from "../gen/game_pb";
+import { userIdentifier } from "./Auth";
 
 function createGameStateStore() {
     const { subscribe, update } = writable(initialGameState());
@@ -19,21 +20,21 @@ export const columns = derived(gameState, $gameState => $gameState.columns);
 
 export const playerMap = derived(gameState, $gameState => $gameState.playerMap);
 
-export const factions = derived(playerMap, $playerMap => {
-    const factions: Map<Faction, Player[]> = new Map([
-        [Faction.RED, []],
-        [Faction.YELLOW, []]
-    ]);
-    $playerMap.forEach(player => factions.get(player.faction)?.push(player));
-    factions.forEach(players => 
-        players.sort((p1, p2) => Number(p2.disconnected) - Number(p1.disconnected))
+export const playersByFaction = derived(playerMap, $playerMap => {
+    const result: Map<Faction, Player[]> = new Map(factions.map(f => [f, []]));
+    $playerMap.forEach(player => result.get(player.faction)!.push(player));
+    result.forEach(
+        players => players.sort((p1, p2) => Number(p2.disconnected) - Number(p1.disconnected))
     );
-    return factions;
+    return result;
 });
 
-export const player: Writable<Player> = writable({
-    identifier: 'Player1',
-    displayName: 'JoJo',
-    disconnected: false,
-    faction: Faction.RED
+export const player: Readable<Player|null> = derived([playerMap, userIdentifier], ([$playerMap, $userIdentifier]) => {
+    if ($userIdentifier == null) {
+        return null;
+    }
+    $playerMap.forEach((player) => console.log(player));
+    return $playerMap.get($userIdentifier) ?? null;
 });
+
+export const faction: Writable<Faction|null> = writable(null);
