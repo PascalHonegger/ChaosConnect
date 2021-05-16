ffafef07dee63990189b691518148a0793c5b638
Pascal Honegger <pascal.honegger@bluewin.ch>
2021-05-16 17:32:13 +0200

    feat(doppio): add simple user help

---
+++ frontend/src/components/GameHelp.svelte
@@ -0,0 +1,11 @@
+<div class="card">
+    <h2>How to Play</h2>
+    <p>The general rules are similar to Connect 4, but some changes were made to incorporate the massive multiplayer aspect:</p>
+    <ul>
+        <li>Clicking on a column will place a piece above it</li>
+        <li>After a random delay it will fall down, allowing you to place your next piece</li>
+        <li>If four pieces of the same faction are connected horizontally, vertically or diagonally the involved players score points</li>
+        <li>Columns automatically get disabled and later cleared once a score was made</li>
+        <li>When more players join, the playing field adapts and automatically grows or shrinks in size</li>
+    </ul>
+</div>
